d03d0160683793507ed93329e1731352fdd75648
Ali <alibahaa786@gmail.com>
2023-09-21 15:35:36 -0700

    Completed swap taskx

--- functions_2.c
+++ functions_2.c
@@ -42,7 +42,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 
 	if (!current->next)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	while (current->next)
