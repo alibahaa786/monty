6c1104198d9d38893b2a8076945ea101794716d4
Ali <alibahaa786@gmail.com>
2023-09-21 08:04:01 -0700

    Fixed betty

--- monty.h
+++ monty.h
@@ -37,7 +37,7 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void (*get_function(char *line, unsigned int line_number))(stack_t **stack, unsigned int line_number);
+void (*get_function(char *line, int line_number))(stack_t **stack, unsigned int line_number);
 void _push(stack_t **stack, unsigned int line_number);
 char **tokenize(char *command);
 void _pall(stack_t **stack, unsigned int __attribute__((unused)) line_number);
