e36e8ce01783d2007c39c6aebca34856d0e9a223
Ali <alibahaa786@gmail.com>
2023-09-21 09:31:11 -0700

    Done

--- ali.m
+++ ali.m
@@ -1,17 +1,9 @@
-push 1
-push 2
-push 3
+push 0 hola
+push 1 estudiante
+push 2 de
+push 3 holberton
+pall    
 push 4
-push 0
-push 110
-push 00
-push 110
-push 111
-push 116
-push 114
-push 101
-push 98
-push 108
-push 111
-push 72
-pstr
+push 5    
+       push    6        
+pall
--- main.c
+++ main.c
@@ -1,113 +1,29 @@
 #include "monty.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-char **commandv = NULL;
+char **op_toks = NULL;
 
 /**
- * main - monty interpeter
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: SUCCESS or FAILURE
-*/
-
-int main(int argc, char *argv[])
+ * main - the entry point for Monty Interp
+ *
+ * @argc: the count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
+ *
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
+ */
+int main(int argc, char **argv)
 {
-	stack_t *stack = NULL;
-	FILE *f;
-	char *command;
-	int line_num = 0;
-	/*void (*func)(stack_t**, unsigned int);*/
+	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
 
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	f = fopen(argv[1], "r");
-	if (!f)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	command = malloc(sizeof(char) * 1024);
-	if (!command)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	while (fgets(command, 1024, f))
-	{
-		line_num++;
-		if (is_blank(command))
-			continue;
-		command[strlen(command) - 1] = '\0';
-		commandv = tokenize(command);
-		if (!commandv)
-			continue;
-		get_function(commandv[0], line_num)(&stack, line_num);
-	}
-	exit(EXIT_SUCCESS);
-}
-
-/**
- * tokenize - split command into words
- * @command: full command
- * Return: array of words of the command
-*/
-
-char **tokenize(char *command)
-{
-	size_t i, j = 0;
-	char *cmd = malloc(sizeof(char *));
-
-	commandv = malloc(sizeof(char **));
-	if (!commandv || !cmd)
-	{
-		fprintf(stderr, "USAGE: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	for (i = 0; command[i]; i++)
-	{
-		if (command[i] != ' ' && command[i - 1] == ' ')
-		{
-			cmd[j] = ' ';
-			j++;
-			cmd[j] = command[i];
-			j++;
-		}
-		else if (command[i] != ' ')
-		{
-			cmd[j] = command[i];
-			j++;
-		}
-	}
-	if (!cmd)
-		return (NULL);
-	commandv[0] = strtok(cmd, " ");
-	for (i = 1; ; i++)
-	{
-		commandv[i] = strtok(NULL, " ");
-		if (!commandv[i])
-			break;
-	}
-	return (commandv);
-}
-
-/**
- * is_blank - check if line of opcode is blank
- * @line: opcode
- * Return: 1 if blank else 0
-*/
-
-int is_blank(char *line)
-{
-	size_t i;
-
-	if (strlen(line) < 2)
-		return (1);
-	for (i = 0; i < strlen(line) - 1; i++)
-	{
-		if (line[i] != ' ')
-			return (0);
-	}
-	return (1);
+		return (usage_error());
+	script_fd = fopen(argv[1], "r");
+	if (script_fd == NULL)
+		return (f_open_error(argv[1]));
+	exit_code = run_monty(script_fd);
+	fclose(script_fd);
+	return (exit_code);
 }
